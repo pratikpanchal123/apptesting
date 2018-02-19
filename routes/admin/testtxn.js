65e9842854876db59fbadfa2a3e3a5a4bef99956
pratik.panchal <pratik.panchal@infostretch.com>
2018-02-19 11:09:03 +0530

    new router

--- routes/admin/testtxn.js
+++ routes/admin/testtxn.js
@@ -36,5 +36,31 @@ res.render('testtxn.ejs',{'config' : config});
         console.log("POST Order end");
 
  });
+    app.post('/getChecksum',function(req, res) {
+        console.log("POST Order start");
+        var paramlist = req.body;
+        var paramarray = new Array();
+        console.log(paramlist);
+        for (name in paramlist)
+        {
+            if (name == 'PAYTM_MERCHANT_KEY') {
+                var PAYTM_MERCHANT_KEY = paramlist[name] ;
+            }else
+            {
+                paramarray[name] = paramlist[name] ;
+            }
+        }
+        console.log(paramarray);
+        paramarray['CALLBACK_URL'] = 'https://paytmapptesting.herokuapp.com/response';  // in case if you want to send callback
+        console.log(PAYTM_MERCHANT_KEY);
+        checksum.genchecksum(paramarray, PAYTM_MERCHANT_KEY, function (err, result)
+        {
+            console.log(result);
+            res.json({ 'checksum' : result });
+        });
+
+        console.log("POST Order end");
+
+    });
 //vidisha
 };
