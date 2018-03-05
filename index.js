5cd9715d216b09bbc2dbcf3e594b006c5431ffac
pratik.panchal <pratik.panchal@infostretch.com>
2018-03-05 12:55:49 +0530

    index

---
+++ index.js
@@ -0,0 +1,27 @@
+
+var express = require(express)
+  , redirect = require(express-redirect);
+ 
+var app = express();
+redirect(app); 
+var router = express.Router();
+var bodyParser = require('body-parser');
+var server = require('http').createServer(app);
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+  extended true
+}));
+
+app.set('port', (process.env.PORT  5000));
+app.use(router);
+require('.routesadmintesttxn')(app);
+require('.routesadminpgredirect')(app);
+require('.routesadminresponse')(app);
+app.use(express.static(__dirname + 'public'));
+app.set('views', __dirname + 'views');
+app.set('view engine', 'ejs');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
